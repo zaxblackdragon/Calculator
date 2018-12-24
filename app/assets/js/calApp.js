283293a92df30bd797a5043a1131353f12245f2f
Zac Seewald <zaxblackdragon.com>
2018-12-24 01:02:54 -0600

    started the js operations. Captures the first number and prints the equation line

--- app/assets/js/calApp.js
+++ app/assets/js/calApp.js
@@ -1,3 +1,18 @@
-$(document).ready(){
-    
-};
+$(document).ready(function () {
+    var firstNum = 0;
+    var secondNum = 0;
+    var isCalculated = false;
+    var isOperatorChosen = false;
+// creates the first number if no operator has been chosen
+    $(".btn-num").on("click", function () {
+        if (isCalculated) {
+            return false;
+        } else if (isOperatorChosen) {
+            secondNum += $(this).val();
+            console.log(secondNum); 
+        } else {
+        firstNum += $(this).val();
+        $(".answer-display").text(firstNum);
+        }
+    });
+});
