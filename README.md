ead53b94164e5575341f12d26eabef0f948e8ada
Zac Seewald <zaxblackdragon.com>
2018-12-27 23:00:05 -0600

    Calculator functions are all working and displaying both the equation and solution to the DOM

--- README.md
+++ README.md
@@ -1,25 +1,32 @@
 # Calculator App
 
-The purpose of this app is practicing a short build from start to finish in one sitting.   
+The purpose of this app is practicing a short build from start to finish in one sitting. 
+
+-Practicing jquery
+-Streamlining Bootstrap usage
+-Using a switch() statement
 
 ## Functionality:
 
-It Should:
 - add
 - subtract
 - multiply 
 - divide
 
 ### HTML
-HTML page will have buttons with the values of 1-9, operators and an equals sign that will complete each equation.
+HTML page uses buttons with the values of 1-9, operators and an equals sign that will complete each equation and clear button to rest the page regardless of the state.
 
 ### Styles
-The layout will be done with a linked stylesheet (style.css) and the Bootstrap cdn for the grid system.
+The layout is done with a linked stylesheet (style.css) and the Bootstrap cdn for the grid system.
 
-### JS
-Javascript file will link to the above html file to complete each operator's function and add each solution to the html page using jquery
+### JS/JQuery
+Javascript file links to the above html file.   
+It completes each operator's function while adding each equation and solution to the html page using jquery.
 
-### Routes
+### Persistance
+In it's current stage, this app has no persistance.
 
-There will be a js file for the server and html routs.
+### Deployment
+Deployed on Heroku:
+
 
--- app/assets/js/calApp.js
+++ app/assets/js/calApp.js
@@ -1,18 +1,94 @@
 $(document).ready(function () {
     var firstNum = 0;
     var secondNum = 0;
+    var operator = "";
     var isCalculated = false;
     var isOperatorChosen = false;
-// creates the first number if no operator has been chosen
+
+    function initCalculator() {
+        firstNumber = "";
+        secondNumber = "";
+        operator = "";
+        isOperatorChosen = false;
+        isCalculated = false;
+        results = 0;
+        $(".equation-display, .solution-display").empty();
+      };
+
+// creates the first number if no operator has been chosen, then the second number after choosing the operator
     $(".btn-num").on("click", function () {
+    // checks to see if the calculation has been run
         if (isCalculated) {
             return false;
+    // If there is an operator already chosen, it will write secondNum
         } else if (isOperatorChosen) {
             secondNum += $(this).val();
-            console.log(secondNum); 
+    // once the operator is selected, then the secondNum gets written and added to the display, showing the equation that has been built
+            $(".equation-display").append(" " + parseInt(secondNum));
+    // if no calculation and no operator, then create the firstNum
         } else {
-        firstNum += $(this).val();
-        $(".answer-display").text(firstNum);
+            firstNum += $(this).val();
+            $(".equation-display").text(parseInt(firstNum));
         }
     });
+
+// Chooses the operator and displays in the html
+    $(".btn-operator").on("click", function () {
+        // Check if a first number has already been selected
+        // Or we've already run a calculation, if so we just exit.
+        if (!firstNum || isCalculated) {
+            return true;
+        };
+        // isOperatorChosen is set to true in order to start the second number
+        isOperatorChosen = true;
+        // stores the value of the operator when the button is clicked
+        operator = $(this).val();
+        // Displays the start of the equation ie firstNum and operator to the html
+        $(".equation-display").text(parseInt(firstNum) + " " + operator);
+    });
+
+// Executes the equation and displays the final solution right of the "=".
+    $(".btn-equal").on("click", function () {
+        // prevent recalculation over and over again
+        if (isCalculated) {
+            return false;
+        };
+        // Disabble the user's option of multiple clicks on the "=" button        
+        isCalculated = true;
+        
+        // Convert string inputs to numbers for calculation
+        firstNum = parseInt(firstNum, 10);
+        secondNum = parseInt(secondNum, 10);
+        
+        // Switch statement to execute the desired equation
+        switch (operator) {
+            case "+":
+                results = firstNum + secondNum;
+                $(".solution-display").html(results)
+                    break;
+            case "-":
+                results = firstNum - secondNum;
+                $(".solution-display").html(results)
+                    break;
+            case "x":
+                results = firstNum * secondNum;
+                $(".solution-display").html(results)
+                    break;
+            case "/":
+                results = firstNum / secondNum;
+                $(".solution-display").html(results)
+                    break;
+        }
+        // adds the "=" to the equation display
+        $(".equation-display").append(" =");
+    });
+
+    // Clear button emptys the fields and resets the start of the process again
+    $(".btn-clear").on("click", function () {
+        // reloads the page everytime to clear any initial inputs
+        location.reload();
+        // initialises the app
+        initCalculator();
+    })
+    initCalculator();
 });
