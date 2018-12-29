e0e2d424b3b33980feebc36cd95f7a70d9e8f8f3
Zac Seewald <zaxblackdragon.com>
2018-12-28 23:59:47 -0600

    fixed file routing error with the js and css files.

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -4,6 +4,6 @@ const path = require("path");
 module.exports = function(app) { 
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../views/index.html"));
+    res.sendFile(path.join(__dirname, "../views/index.html"));
   });
 };
--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const PORT = process.env.PORT || 8080;
 
 
 // Middleware
+app.use(express.static(__dirname + "/public"));
+//app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, './app/public')));
 app.use(express.json());
 
 // this imports the routes from the htmlRoutes.js file
-const htmlRoutes = require("./app/routes/htmlroutes.js");
-htmlRoutes(app);
+const routes = require("./routes/htmlroutes.js");
+routes(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT and so is yo mama!!!" + PORT);
